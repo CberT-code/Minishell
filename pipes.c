0d2c10738c92ded0cba3ecccde2ed0623a0c831f
CberT-code <cyrille.bertola@gmail.com>
2020-06-10 20:33:43 +0200

    redirection are now correct, Only have to make it cleaner with a struct

--- pipes.c
+++ pipes.c
@@ -1,75 +1,94 @@
+#include <sys/wait.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 #include <sys/stat.h>
+#include <stdlib.h>
+
+void  init_pipes(int nb_pipes, int *pipes)
+{
+  int i;
+  int j;
+
+  i = -1;
+  j = 0;
+  while (++i < nb_pipes)
+  {
+    pipe(pipes + j);
+    j += 2;
+  }
+}
+
+void  close_pipes(int nb_pipes, int *pipes)
+{
+  int i;
+
+  i = -1;
+  while (++i < nb_pipes)
+    close(pipes[i]);
+}
+
+void  wait_pipes(int nb_cmd, pid_t *pid, int *ret)
+{
+  int i;
+
+  i = -1;
+  while (++i < nb_cmd)
+    waitpid(pid[i], ret, 0);
+}
+
+void do_dup(int j, int nb_cmd, int *pipes, int redir, char *redir_extern, char *redir_intern)
+{
+
+  if (j > 0)
+    dup2(pipes[j * 2 - 2], 0);
+  if (j < nb_cmd - 1 || redir_extern != NULL)
+  {
+    if (redir == 1 && redir_extern != NULL)
+      pipes[j * 2 + 1] = open(redir_extern, O_RDONLY | O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
+    if (redir == 2 && redir_extern != NULL)
+      pipes[j * 2 + 1] = open(redir_extern, O_RDONLY |O_WRONLY | O_APPEND | O_CREAT, S_IRUSR | S_IWUSR);
+    dup2(pipes[j * 2 + 1], 1);
+  }
+  if (redir_intern != NULL)
+      dup2(open(redir_intern, O_RDONLY), 0);
+}
+
+void do_pipe(char ***all, int nb_cmd, int *ret, char **redir_extern, char **redir_intern)
+{
+  pid_t   pid[nb_cmd + 1];
+  int     pipes[nb_cmd * 2];
+  int     j = -1;
+  int     status;
+
+  init_pipes(nb_cmd * 2, pipes);
+  while (++j < nb_cmd)
+  {
+    if (!(pid[j] = fork()))
+    {
+      do_dup( j, nb_cmd, pipes, 3, redir_extern[j], redir_intern[j]);
+      close_pipes(nb_cmd * 2, pipes);
+      if ((*ret = execvp(*all[j], all[j])))
+        exit(-1);
+    }
+  }
+  close_pipes(nb_cmd * 2, pipes);
+  wait_pipes(nb_cmd * 2, pid, ret);
+}
 
 int main(int argc, char **argv)
 {
-  int status;
-  int i;
+  int ret;
 
-  char *cat_args[] = {"cat", "pipes.c", NULL};
-  char *grep_args[] = {"grep", "cat", NULL};
-  char *cut_args[] = {"cut", "-b", "1-10", NULL};
-
-  int pipes[4];
-  pipe(pipes);
-  pipe(pipes + 2);
-  
-  if (fork() == 0)
-    {
-      dup2(pipes[1], 1);
-
-      close(pipes[0]);
-      close(pipes[1]);
-      close(pipes[2]);
-      close(pipes[3]);
-
-      execvp(*cat_args, cat_args);
-    }
-  else
-    {
-
-      if (fork() == 0)
-	{
-	  
-	  dup2(pipes[0], 0);
-
-	  dup2(pipes[3], 1);
-
-	  close(pipes[0]);
-	  close(pipes[1]);
-	  close(pipes[2]);
-	  close(pipes[3]);
-
-	  execvp(*grep_args, grep_args);
-	}
-      else
-	{
-
-	  if (fork() == 0)
-	    {
-
-	      dup2(pipes[2], 0);
-
-	      close(pipes[0]);
-	      close(pipes[1]);
-	      close(pipes[2]);
-	      close(pipes[3]);
-
-	     if (execvp(*cut_args, cut_args))
-            exit;
-	    }
-	}
-    }
-  
-  close(pipes[0]);
-  close(pipes[1]);
-  close(pipes[2]);
-  close(pipes[3]);
-
-  for (i = 0; i < 3; i++)
-    wait(&status);
+  char *test17[] = {"head", "-n", "5", NULL};
+  char *test18[] = {"grep", "d", NULL};
+  char *test19[] = {"cut", "-b", "1-10", NULL};
+  char *test20[] = {"cut", "-b", "2-5", NULL};
+  char **all5[4] = {test17, test18, test19, test20};
+  char *redir_extern[] = {NULL, NULL, NULL, NULL};
+  char *redir_intern[] = {"test.c", NULL, NULL, NULL};
+  do_pipe(all5, 4, &ret, redir_extern, redir_intern);
+  printf("\n\nRET = %d\n", ret);
+  return (0);
 }
