660405ba26b24ebc1f20e4ba3f3abd895ddd4c69
CberT-code <cyrille.bertola@gmail.com>
2020-06-19 15:08:06 +0200

    20.06.19 15:08:06

---
+++ srcs/fcts/export.c
@@ -0,0 +1,31 @@
+#include "../../minishell.h"
+
+char         **ft_export(char *str, char **tri_selectif)
+{
+    int i;
+    char **tableau_des_variables_d_environnement;
+
+    i = 0;
+    while (tri_selectif[i])
+        i++;
+    if (!(tableau_des_variables_d_environnement = malloc(sizeof(char *) * i + 2)))
+        exit(0);
+    i = -1;
+    while (tri_selectif[++i])
+        tableau_des_variables_d_environnement[i] = ft_strncpy(tri_selectif[i], ft_strlen(tri_selectif[i]));
+    tableau_des_variables_d_environnement[i++] = ft_strncpy(str, ft_strlen(str));
+    tableau_des_variables_d_environnement[i++] = '\0';
+    return (tableau_des_variables_d_environnement);
+}
+
+int main(int argc, char **argv, char **envp)
+{
+	char *test = "zozo=popo";
+    char **tab;
+    int i = -1;
+
+    tab = ft_export(test, envp);
+	while (tab[++i])
+        printf("%s\n", tab[i]);
+	return (0);
+}
