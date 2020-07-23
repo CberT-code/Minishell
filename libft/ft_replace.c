8831f59bc008ea07ef40f643bb5303ffd22cb320
CberT-code <cyrille.bertola@gmail.com>
2020-07-23 17:16:35 +0200

    20.07.23 17:16:35

--- libft/ft_replace.c
+++ libft/ft_replace.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_replace.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cyrillebertola <cyrillebertola@student.    +#+  +:+       +#+        */
+/*   By: cbertola <cbertola@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/24 14:21:44 by cyrillebert       #+#    #+#             */
-/*   Updated: 2020/03/24 14:26:09 by cyrillebert      ###   ########.fr       */
+/*   Updated: 2020/07/23 17:14:16 by cbertola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
